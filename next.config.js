cbc36b132a8762db6d08578c921a8f2e8697b4c3
willy-moses <willkapenamowa@gmail.com>
2025-11-29 23:20:04 +0200

    Configure for Render deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/bosele2' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/bosele2/' : '',
   images: {
     unoptimized: true
   }
